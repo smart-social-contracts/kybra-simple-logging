9a554aa019739a087c59271c05e44499b94b111f
Jose Perez <jmpmonteagudo@gmail.com>
2025-03-29 09:50:22 +0100

    wip building tests

--- tests/src/main.py
+++ tests/src/main.py
@@ -1,8 +1,32 @@
 from kybra import ic, query
 
-from kybra_simple_logging import logger
+from kybra_simple_logging import logger, set_log_level, get_logger
+
+
+def logging_test(l: "SimpleLogger"):
+    ic.print('******************')
+    l.debug('Hello from DEBUG in ' + l.name)
+    l.info("Hello from INFO in " + l.name)
+    l.warning('Hello from WARNING in ' + l.name)
+    l.error('Hello from ERROR in ' + l.name)
+    l.critical('Hello from CRITICAL in ' + l.name)
+
 
 @query
 def greet() -> str:
-    logger.info("Hello from the logger!")
-    return "Hello!"
+    logging_test(logger)
+    set_log_level("DEBUG")
+    logging_test(logger)
+
+    
+    my_logger_1 = get_logger("my_logger_1")
+    logging_test(my_logger_1)
+    logging_test(logger)
+
+    set_log_level("ERROR", "my_logger_1")
+    logging_test(my_logger_1)
+    logging_test(logger)
+    
+
+    return "end"
+
--- tests/src/tests/test_example_1.py
+++ tests/src/tests/test_example_1.py
@@ -1,18 +1,18 @@
 
-from kybra_simple_logging import logging
+# from kybra_simple_logging import logging
 
 
-def run():
+# def run():
 
 
-    logging.debug("This is a DEBUG message")
-    logging.info("This is an INFO message")
-    logging.warning("This is a WARNING")
-    logging.error("This is an ERROR")
-    print('test_1 passed')
+#     logging.debug("This is a DEBUG message")
+#     logging.info("This is an INFO message")
+#     logging.warning("This is a WARNING")
+#     logging.error("This is an ERROR")
+#     print('test_1 passed')
 
-    return 0
+#     return 0
 
 
-if __name__ == "__main__":
-    exit(run())
+# if __name__ == "__main__":
+#     exit(run())
